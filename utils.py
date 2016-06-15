858cbd1bf1dbfbda5768dc3683d0ceddbc47fb80
Thomas Adriaan Hellinger <brotherjack1877@gmail.com>
2016-06-15 14:50:46 -0400

    Simplified the safe_list_append and added documentation

--- utils.py
+++ utils.py
@@ -2,23 +2,18 @@
 Created on Jun 13, 2016
 
 @author: Thomas Adriaan Hellinger
+
+Utility file for operations that are useful across modules and packages.
 '''
-
-def safe_list_append(x,y):
-    if type(x) is list:
-        if type(y) is list:
-            return x + y
-        else:
-            return x + [y]
-    else:
-        if type(y) is list:
-            return [x] + y 
-        else:
-            return [x] + [y]
-
 def flatten(l): 
+    """Flattens a list into 1-dimension"""
     return flatten(l[0]) +\
         (flatten(l[1:]) if len(l) > 1 else []) if type(l) is list else [l]
 
 def listize(x):
+    """Returns a list with a thing in it, unless the thing is already a list"""
     return [x] if type(x) is not list else x
+
+def safe_list_append(x,y):
+    """Appends a list of x to a list of y"""
+    return listize(x) + listize(y) 
