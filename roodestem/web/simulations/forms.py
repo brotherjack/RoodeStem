8ee94f55ac0a23b02a93b454e7c73cf0d1d7303c
Thomas Adriaan Hellinger <brotherjack1877@gmail.com>
2016-08-10 23:38:47 -0400

    Ability to add candidate field on client side, but doesn't effect the simulation, and the remove candidate button doesn't work either

--- roodestem/web/simulations/forms.py
+++ roodestem/web/simulations/forms.py
@@ -6,11 +6,13 @@ Created on Aug 8, 2016
 import sys
 
 from flask_wtf import Form
-from wtforms.fields import IntegerField, SubmitField
+from wtforms.fields import IntegerField, SubmitField, FieldList, StringField
 from wtforms.validators import NumberRange, InputRequired
 
 
 class RandomCondorcetForm(Form):
+    candidates = FieldList(StringField('Candidate'), 'Candidates',
+                           min_entries=2, max_entries=100)
     number_of_voters = IntegerField(
         'Number of Voters', 
         validators=[InputRequired(),NumberRange(2,10)]
@@ -18,5 +20,10 @@ class RandomCondorcetForm(Form):
     seed_field = IntegerField(
         "Seed", 
         validators=[NumberRange(-1*sys.maxsize, sys.maxsize)]
-    ) 
+    )
     submit_run = SubmitField("Run")
+    
+#     def __init__(self, choices=['a', 'b', 'c', 'd']):
+#         super()
+#         for choice in choices:
+#             self.candidates.append_entry(StringField("Candidate", choice))
