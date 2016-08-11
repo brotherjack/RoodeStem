84a56a07b0bd51554f0a79431a665fc5b3fc6f0e
Thomas Adriaan Hellinger <brotherjack1877@gmail.com>
2016-08-11 15:04:55 -0400

    Fixed up RandomCondorcetDemo interface so that added candidates can be removed; not attached to anything and may need additional logic to rename fields post-delete

--- roodestem/web/static/js/random_condorcet.js
+++ roodestem/web/static/js/random_condorcet.js
@@ -1,18 +1,18 @@
-$(function() {
+$(document).ready(function() {
   var candidateTable = $('#candidate_table');
-  var i = $('#candidate_table tr').length + 2;
+  var i = $('#candidate_table tr').length;
         
   $('#add_candidate').on('click', null, function() {
     if(i == 100){
       return;
     }
-    $('<tr><td><label for="candidates-' + i+ '">Candidate</label>:</td><td><input class="form-control" id="candidates-1" name="candidates-' + i+ '" type="text" value=""></td><td><button type="button" class="btn btn-default btn-sm" id="remove_candidate"><span class="glyphicon glyphicon-trash" aria-hidden="true"></span></button></td></tr>').appendTo(candidateTable);
+    $('<tr><td><label for="candidates-' + i+ '">Candidate</label>:</td><td><input class="form-control" id="candidates-' + i+ '" name="candidates-' + i+ '" type="text" value=""></td><td><button type="button" class="btn btn-default btn-sm remove_candidate"><span class="glyphicon glyphicon-trash" aria-hidden="true"></span></button></td></tr>').appendTo(candidateTable);
     i++;
     return false;
   });
         
-  $('#remove_candidate').on('click', null, function() { 
-    $(this).parents('td').remove();
+  $(document).on('click', 'button.remove_candidate', function() { 
+    $(this).parents('tr').remove();
     i--;
     return false;
   });
